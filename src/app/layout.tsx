6b23d4adfdd64a2469b66d039fc5c4e6d5bd9e1a
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2025-03-07 10:09:45 +0100

    gt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import { GoogleTagManager } from "@next/third-parties/google";
-import Script from 'next/script';
-
+import { GoogleTagManager } from "@next/third-parties/google";
 import LandingPageHeader from "@/components/landingpage/Header";
 import Footer from "@/components/landingpage/Footer";
-
 
 export const metadata: Metadata = {
   title: "Kite App",
@@ -19,29 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      <>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-         function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MQD3GK9P');
-        `}
-      </Script>
-    </>
-      </head>
       <body className={`no-scrollbar`}>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQD3GK9P"
-            height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
-        </noscript>
         <LandingPageHeader />
         {children}
         <Footer />
       </body>
-      {/* <GoogleTagManager gtmId={"G-9ET789RPSE"} /> */}
+      <GoogleTagManager gtmId={"GTM-MQD3GK9P"} />
     </html>
   );
 }
